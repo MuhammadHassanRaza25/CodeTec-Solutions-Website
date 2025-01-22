b06bb8fba5e385715a07b7fb73fea5d4c5fd99f7
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2025-01-22 18:45:03 +0500

    working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import Footer from './Footer'
 import Header from './Header'
 import HeroSection from './HeroSection'
-import Services from './services'
+import Services from './Services'
 
 function Home() {
 
